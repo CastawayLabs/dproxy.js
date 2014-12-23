2782d81395d92013a3af637b09a93c2c16f23a05
DarkSeraphim <datfireblast709@gmail.com>
2014-12-23 04:42:28 +0100

    FIX: Echo x-forwarded-for header for testing If the x-forwarded-for header is available, echo it so the mocha tests can see whether the field was successfully added or not

--- bin/test_server.js
+++ bin/test_server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.writeHead(200, {'Content-Type': 'text/plain', 'X-Forwarded-For': req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'] : 'null'});
 	res.end('Hello World\n');
 }).listen(1337, '127.0.0.1');
